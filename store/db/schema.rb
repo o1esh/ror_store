3fdaa3b1339ce9a86978c153ed07d5e2d6a234d8
Ablimit <you@example.com>
2018-11-14 12:48:29 +0600

    video7

--- store/db/schema.rb
+++ store/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_074103) do
+ActiveRecord::Schema.define(version: 2018_11_07_075526) do
 
   create_table "items", force: :cascade do |t|
     t.float "price"
     t.string "name"
     t.boolean "real"
     t.float "wight"
+    t.text "description"
     t.string "timestamps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
